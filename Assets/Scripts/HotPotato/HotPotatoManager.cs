01d1162241fda08c895370b7b606b0c42f740fc6
luuk-royal <luukkaalverink@gmail.com>
2023-11-05 13:49:43 +0200

    Finished the hot potato game

--- Assets/Scripts/HotPotato/HotPotatoManager.cs
+++ Assets/Scripts/HotPotato/HotPotatoManager.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
+using TMPro;
 
 public class Potato : MonoBehaviour
 {
-    [SerializeField] private ER_GameManager gameManager;
+    public ER_GameManager gameManager;
+
+    public TextMeshProUGUI playerOneText;
+    public TextMeshProUGUI playerTwoText;
+    public TextMeshProUGUI timeLeftText;
 
     private Player holdingPlayer;
 
     private Vector3 positionOne = new Vector3(-5f, 0, 0);
     private Vector3 positionTwo = new Vector3(4.5f, 0, 0);
 
-    private float timeHeld = 0;
-    private float timer = 0;
+    private float scoreTimePlayerOne;
+    private float scoreTimePlayerTwo;
+    private float timeHeld;
+    private float timer;
+    private float totalTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreTimePlayerOne = 0;
+        scoreTimePlayerTwo = 0;
+
+        playerOneText.text = $"{scoreTimePlayerOne} S";
+        playerTwoText.text = $"{scoreTimePlayerTwo} S";
+
+        timeHeld = 0;
+        totalTime = 30;
+
+        timeLeftText.text = $"{totalTime} S";
+
         holdingPlayer = Player.Player1;
         transform.position = positionOne;
         timer = Random.Range(5.0f, 10.0f);
@@ -23,6 +42,8 @@ public class Potato : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeLeftText.text = $"{totalTime} S";
+
         timeHeld += Time.deltaTime;
         if (timeHeld >= timer)
         {
@@ -46,6 +67,40 @@ public class Potato : MonoBehaviour
         {
             ThrowPotato(Player.Player2);
         }
+
+        totalTime -= Time.deltaTime;
+        if (totalTime < 0)
+        {
+            timeOut();
+        }
+    }
+
+    public void timeOut()
+    {
+        if (holdingPlayer == Player.Player1)
+        {
+            if (timeHeld > scoreTimePlayerOne)
+            {
+                scoreTimePlayerOne = timeHeld;
+                playerOneText.text = $"{scoreTimePlayerOne} S";
+            }
+        }
+        else
+        {
+            if (timeHeld > scoreTimePlayerTwo)
+            {
+                scoreTimePlayerTwo = timeHeld;
+                playerTwoText.text = $"{scoreTimePlayerTwo} S";
+            }
+        }
+
+        if (scoreTimePlayerOne > scoreTimePlayerTwo)
+        {
+            gameManager.SelectWinner(Player.Player1);
+        } else
+        {
+            gameManager.SelectWinner(Player.Player2);
+        }
     }
 
     public void ThrowPotato(Player player)
@@ -55,11 +110,25 @@ public class Potato : MonoBehaviour
             if (player == Player.Player2) { 
                 transform.position = positionOne;
                 holdingPlayer = Player.Player1;
+
+                if (timeHeld > scoreTimePlayerTwo)
+                {
+                    scoreTimePlayerTwo = timeHeld;
+                    playerTwoText.text = $"{scoreTimePlayerTwo} S";
+                }
             } else
             {
                 transform.position = positionTwo;
                 holdingPlayer = Player.Player2;
+
+                if (timeHeld > scoreTimePlayerOne)
+                {
+                    scoreTimePlayerOne = timeHeld;
+                    playerOneText.text = $"{scoreTimePlayerOne} S";
+                }
             }
+
+            timeHeld = 0;
         }
     }
 }
